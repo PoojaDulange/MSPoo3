17540026132fab63180c0d01e3f4480f26a88649
Pooja Dulange <pdpoojadulange5@gmail.com>
2022-09-28 10:00:02 +0530

    update

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require("./api/users/users.router");
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/api/users", usersRouter);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.listen(process.env.APP_PORT, () => {
+  console.log(`server running on port ${process.env.APP_PORT}`);
+});
 
 module.exports = app;
